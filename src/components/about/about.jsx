707b4b09622d2c63f676dd9c9d24576660e1c056
Aymansaeed9542 <aym9542@gmail.com>
2025-08-03 11:39:17 +0300

    Updated About page content and improved styling

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import style from './about.module.css';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className={`${style.about} d-flex justify-content-center align-items-center flex-column text-white`}>
+      <h1>ABOUT COMPONENT</h1>
+            <div className='d-flex justify-content-center align-items-center mb-3'>
+              <div className={`${style.line} me-3`}></div>
+              <i className='fa-solid fa-star'></i>
+              <div className={`${style.line} ms-3`}></div>
+            </div>
+            <div className={`d-flex flex-row justify-content-center align-items-center ${style.content} `}>
+              <div><p>Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.
+                </p></div>
+              <div><p>Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.
+                </p></div>
+            </div>
+    </div>
   )
 }
 
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -8,6 +8,11 @@ const Home = () => {
         <img src={img} alt="Avatar" />
       </div>
       <h1>START FRAMEWORK</h1>
+      <div className='d-flex justify-content-center align-items-center mb-3'>
+        <div className={`${style.line} me-3`}></div>
+        <i className='fa-solid fa-star'></i>
+        <div className={`${style.line} ms-3`}></div>
+      </div>
       <p>Graphic Artist - Web Designer - Illustrator</p>
     </div>
   );
