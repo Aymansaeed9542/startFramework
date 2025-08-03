32d36103ef24dfa0705edbfbe7be34cae43f15b1
Aymansaeed9542 <aym9542@gmail.com>
2025-08-04 00:09:06 +0300

    Update About component content and styling

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import style from "./contact.module.css";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div
+      className={`${style.contact} d-flex r align-items-center flex-column `}
+    >
+      <h1>CONTACT SECTION</h1>
+      <div className="d-flex justify-content-center align-items-center mb-3">
+        <div className={`${style.line} me-3 `}></div>
+        <i className="fa-solid fa-star"></i>
+        <div className={`${style.line} ms-3`}></div>
+      </div>
+      <div className={`${style.form}`}>
+        <form className="container">
+          <div className="inputFields d-flex flex-column">
+            <input type="text" className="w-100" placeholder="userName" />
+            <input type="text" className="w-100" placeholder="userAge" />
+            <input type="text" className="w-100" placeholder="userEmail" />
+            <input type="text" className="w-100" placeholder="userMessage" />
+          </div>
+          <div className="button">
+            <button className="btn btn-success mt-3">Send Message</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
